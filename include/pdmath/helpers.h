a12af27c3815ea4d299a33aea81f40e033bfca8f
Derek Huang <djh458@stern.nyu.edu>
2022-06-26 21:24:36 -0400

    helpers.h: update banner format and remove need for auto var for split_idx

--- include/pdmath/helpers.h
+++ include/pdmath/helpers.h
@@ -18,16 +18,25 @@ namespace pdmath {
  *
  * Useful for identifying the program in `examples` being run by the user.
  *
- * @param filename `const char *` name of the program with `main` in `examples`
+ * @param file_path `const char *` path of the program with `main` in `examples`
+ * @param print `bool` where the header is printed if `true`
+ * @returns `std::string` giving the header to print
  */
-inline void print_example_header(const char *filename)
+inline std::string print_example_header(
+  const char *file_path, bool print = true)
 {
-  std::string filename_(filename);
+  std::string abs_path(file_path);
   std::string split_str("math/examples");
-  auto split_idx = filename_.find(split_str);
   // the +1 at the end is to not include the directory slash
-  std::cout << "[" << filename_.substr(split_idx + split_str.size() + 1) << "]";
-  std::cout << std::endl;
+  std::string rel_path = abs_path.substr(
+    abs_path.find(split_str) + split_str.size() + 1
+  );
+  std::string frame(rel_path.size(), '-');
+  std::string header = "+" + frame + "+\n|" + rel_path + "|\n+" + frame + "+";
+  if (print) {
+    std::cout << header << std::endl;
+  }
+  return header;
 }
 
 }  // namespace pdmath
