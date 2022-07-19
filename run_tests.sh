b82c16959a3a068eba660c2281b0fee5ed2ae930
Derek Huang <djh458@stern.nyu.edu>
2022-07-19 00:03:46 -0400

    test/CMakeLists.txt: add note on why gtest_discover_tests is slow af, run_tests.sh: use half available threads to run parallel tests

--- run_tests.sh
+++ run_tests.sh
@@ -6,8 +6,10 @@
 # Main function.
 #
 main() {
-    # GTEST_COLOR=yes allows color output from Google test mains
-    GTEST_COLOR=yes ctest --test-dir build "$@"
+    # GTEST_COLOR=yes allows color output from Google test mains. the nested
+    # command substitution gets half the number of available threads on system.
+    # note that -j x can be overridden with a subsequent -j y.
+    GTEST_COLOR=yes ctest --test-dir build -j $(expr $(nproc --all) / 2) "$@"
 }
 
 set -e
