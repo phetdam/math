eb9e879cddf315d3a535b9688945780d95b80267
Derek Huang <djh458@stern.nyu.edu>
2022-09-01 23:29:10 -0400

    testing/utils.h: add func_type_triple type wrapper to make testing multi-type template callables easier

---
+++ include/pdmath/testing/utils.h
@@ -0,0 +1,30 @@
+/**
+ * @file utils.h
+ * @author Derek Huang
+ * @brief C++ header for some testing utils
+ * @copyright MIT License
+ */
+
+#ifndef PDMATH_TESTING_UTILS_H_
+#define PDMATH_TESTING_UTILS_H_
+
+/**
+ * Templated wrapper for scalar , gradient, and Hessian types.
+ *
+ * Specializations can be used with the Google Test `::testing::Types` template
+ * for templated tests with `TYPED_TEST` in Google Test.
+ *
+ * @tparam T scalar return type
+ * @tparam V_t vector input type/gradient return type, a `*Container*
+ * @tparam M_t Hessian return type, ex. an `Eigen::Matrix` specialization
+ */
+template <typename T, typename V_t, typename M_t>
+class func_type_triple {
+public:
+  func_type_triple() = delete;
+  using scalar_t = T;
+  using vector_t = V_t;
+  using matrix_t = M_t;
+};
+
+#endif  // PDMATH_TESTING_UTILS_H_
