b9f25543f01f45b0bd0c8aa4b81545227b2cad7d
Derek Huang <djh458@stern.nyu.edu>
2022-09-10 02:04:49 -0400

    helpers_test.cc: rewrite to be more generic and used TYPED_TEST to test (boost|eigen)_vector_from

--- test/helpers_test.cc
+++ test/helpers_test.cc
@@ -8,6 +8,8 @@
 #include "pdmath/helpers.h"
 
 #include <sstream>
+#include <type_traits>
+#include <utility>
 #include <vector>
 
 #include <Eigen/Core>
@@ -17,19 +19,24 @@
 #include "pdmath/types.h"
 
 /**
- * Values used in the HelpersTest test fixture for populating `std::vector`.
+ * Values used in the `HelpersVt(One|Two)Test` test fixtures for containers.
  *
- * Used later to test the variadic template versions of `boost_vector_from`
- * and `eigen_vector_from` to ensure that they work as intended.
+ * Used later to test the variadic template versions of `boost_vector_from`,
+ * `eigen_vector_from`, `vector_from` to ensure that they work as intended.
  */
 #define HELPERS_TEST_VECTOR_VALUES 1., 34, 4.5, 9.82
+
+/**
+ * Values used in the `EigenVectorFromMatrixTest` test fixture.
+ */
+#define HELPERS_TEST_MATRIX_VALUES {1., 34}, {4.5, 9.82}
 
 namespace {
 
 /**
- * Test fixture class for helper tests.
+ * Test fixture class for miscellaneous helper tests.
  */
-class HelpersTest : public ::testing::Test {
+class HelpersMiscTest : public ::testing::Test {
 protected:
   // name of exercise example path we want to show
   static const std::string ex_name_;
@@ -37,24 +44,19 @@ protected:
   static const std::string ex_path_;
   // frame to display around ex_name_, i.e. a bunch of dashes same length
   static const std::string ex_frame_;
-  static const std::vector<double> values_;
 };
 
-const std::string
-HelpersTest::ex_name_ = "dir1/exampleX.cc";
-
-const std::string
-HelpersTest::ex_path_ = "math/examples/" + HelpersTest::ex_name_;
-
-const std::string
-HelpersTest::ex_frame_ = std::string(HelpersTest::ex_name_.size(), '-');
-
-const std::vector<double> HelpersTest::values_ = {HELPERS_TEST_VECTOR_VALUES};
+const std::string HelpersMiscTest::ex_name_ = "dir1/exampleX.cc";
+const std::string HelpersMiscTest::ex_path_ = "math/examples/" +
+  HelpersMiscTest::ex_name_;
+const std::string HelpersMiscTest::ex_frame_ = std::string(
+  HelpersMiscTest::ex_name_.size(), '-'
+);
 
 /**
  * Test that `print_example_header` overloads work as expected.
  */
-TEST_F(HelpersTest, PrintExampleHeaderTest)
+TEST_F(HelpersMiscTest, PrintExampleHeaderTest)
 {
   auto header = pdmath::print_example_header(ex_path_, false);
   EXPECT_EQ(header, pdmath::print_example_header(ex_path_.c_str(), false));
@@ -64,73 +66,155 @@ TEST_F(HelpersTest, PrintExampleHeaderTest)
 }
 
 /**
+ * Test fixture template for tests requiring a single vector type.
+ *
+ * For example, testing `boost_vector_from`, `eigen_vector_from`.
+ *
+ * @tparam V_t *Container* type
+ */
+template <typename V_t>
+class HelpersVtOneTest : public ::testing::Test {
+protected:
+  static const V_t values_;
+};
+
+// as noted before, using {{}} works for both std::vector and Eigen vectors
+template <typename V_t>
+const V_t HelpersVtOneTest<V_t>::values_{{HELPERS_TEST_VECTOR_VALUES}};
+
+using HelpersVtOneTestTypes = ::testing::Types<
+  pdmath::double_vector,
+  std::vector<float>,
+  Eigen::VectorXf,
+  Eigen::Vector4d,
+  Eigen::RowVectorXd
+>;
+TYPED_TEST_SUITE(HelpersVtOneTest, HelpersVtOneTestTypes);
+
+/**
  * Test that `print_vector` overloads work as expected.
  */
-TEST_F(HelpersTest, PrintVectorTest)
+TYPED_TEST(HelpersVtOneTest, PrintVectorTest)
 {
   // formatted string printed by default
   std::stringstream ss_def;
-  for (const auto& v : values_) {
-    ss_def << v;
-    if (v != values_.back()) {
+  for (const auto& v : TestFixture::values_) {
+    if (v != *TestFixture::values_.begin()) {
       ss_def << " ";
     }
+    ss_def << v;
   }
   // formatted string printed using custom vector_print_policy
   pdmath::vector_print_policy print_policy(",", 2, "[", 2, "]", 2);
   std::stringstream ss_cust;
   ss_cust << "[  ";
-  for (const auto& v : values_) {
-    ss_cust << v;
-    if (v != values_.back()) {
-      ss_cust << ",";
+  for (const auto& v : TestFixture::values_) {
+    if (v != *TestFixture::values_.begin()) {
+      ss_cust << ",  ";
     }
-    ss_cust << "  ";
+    ss_cust << v;
   }
-  ss_cust << "]";
-  EXPECT_EQ(ss_def.str(), pdmath::print_vector(values_, false));
-  EXPECT_EQ(ss_cust.str(), pdmath::print_vector(values_, print_policy, false));
+  ss_cust << "  ]";
+  EXPECT_EQ(ss_def.str(), pdmath::print_vector(TestFixture::values_, false));
+  EXPECT_EQ(
+    ss_cust.str(),
+    pdmath::print_vector(TestFixture::values_, print_policy, false)
+  );
 }
 
 /**
  * Test that `boost_vector_from` overloads work as expected.
  */
-TEST_F(HelpersTest, BoostVectorFromTest)
+TYPED_TEST(HelpersVtOneTest, BoostVectorFromTest)
 {
-  auto bvec1 = pdmath::boost_vector_from(values_);
-  auto bvec2 = pdmath::boost_vector_from<double>(HELPERS_TEST_VECTOR_VALUES);
-  EXPECT_EQ(bvec1.size(), bvec2.size());
+  using value_t = typename TypeParam::value_type;
+  auto bvec1 = pdmath::boost_vector_from<value_t>(HELPERS_TEST_VECTOR_VALUES);
+  auto bvec2 = pdmath::boost_vector_from(TestFixture::values_);
   // boost::numeric::ublas::vector does not define operator==, so use gmock
   EXPECT_THAT(bvec1, ::testing::ElementsAreArray(bvec2));
 }
 
 /**
- * Test that `eigen_vector_from` overloads work as expected.
+ * Test that `eigen_vector_from` overloads work as expected on vectors.
  */
-TEST_F(HelpersTest, EigenVectorFromTest)
+TYPED_TEST(HelpersVtOneTest, EigenVectorFromTest)
 {
-  auto evec1 = pdmath::eigen_vector_from(values_);
-  auto evec2 = pdmath::eigen_vector_from<double>(HELPERS_TEST_VECTOR_VALUES);
+  using value_t = typename TypeParam::value_type;
+  auto evec1 = pdmath::eigen_vector_from<value_t>(HELPERS_TEST_VECTOR_VALUES);
+  auto evec2 = pdmath::eigen_vector_from(TestFixture::values_);
   EXPECT_EQ(evec1, evec2);
-  // eigen_vector_from should work on a matrix
-  Eigen::Matrix2f m2;
-// MSVC complains about truncation from double to const float
-#ifdef _MSC_VER
-#pragma warning (push)
-#pragma warning (disable: 4305)
-#endif  // _MSC_VER
-  m2 << HELPERS_TEST_VECTOR_VALUES;
-#ifdef _MSC_VER
-#pragma warning (pop)
-#endif  // _MSC_VER
-  // need to cast double to float since m2 has float scalar type. of course we
-  // cannot do the reverse since float is lower precision than double
-  EXPECT_EQ(evec1.cast<float>(), pdmath::eigen_vector_from(m2));
-  // should also work on a row vector or a fixed-size vector
-  Eigen::RowVector4d rv{{HELPERS_TEST_VECTOR_VALUES}};
-  EXPECT_EQ(evec1, pdmath::eigen_vector_from(rv));
-  Eigen::VectorXd v{{HELPERS_TEST_VECTOR_VALUES}};
-  EXPECT_EQ(evec1, pdmath::eigen_vector_from(v));
 }
+
+/**
+ * Test fixture template for `eigen_vector_from` tests using Eigen matrices.
+ *
+ * We make this separate from `HelpersVtOneTest` since 2D `Eigen::Matrix`
+ * specializations required incompatible nested initialization.
+ *
+ * @tparam M_t `Eigen::Matrix` specialization
+ */
+template <typename M_t>
+class EigenVectorFromMatrixTest : public ::testing::Test {
+protected:
+  static const M_t values_;
+};
+
+template <typename M_t>
+const M_t EigenVectorFromMatrixTest<M_t>::values_{HELPERS_TEST_MATRIX_VALUES};
+
+using EigenVectorFromMatrixTestTypes = ::testing::Types<
+  Eigen::Matrix2f, Eigen::Matrix2Xd, Eigen::MatrixXf
+>;
+TYPED_TEST_SUITE(EigenVectorFromMatrixTest, EigenVectorFromMatrixTestTypes);
+
+/**
+ * Test that `eigen_vector_from` also works with Eigen matrix typedefs.
+ *
+ * Also checks that the `eigen_vector_from` overload taking an
+ * `Eigen::MatrixBase<Derived>` works as expected on expressions.
+ */
+TYPED_TEST(EigenVectorFromMatrixTest, Test)
+{
+  using value_t = typename TypeParam::value_type;
+  auto evec1 = pdmath::eigen_vector_from<value_t>(HELPERS_TEST_VECTOR_VALUES);
+  // canceling expression resolves to TestFixture::values_
+  auto evec2 = pdmath::eigen_vector_from(
+    TestFixture::values_ - TestFixture::values_ + TestFixture::values_
+  );
+  EXPECT_EQ(evec1, evec2);
+}
+
+/**
+ * Test fixture template for tests requiring two vector types.
+ *
+ * @tparam Tp_t `std::pair<T, U>` with `T`, `U` *Container* types
+ */
+template <typename Tp_t>
+class HelpersVtTwoTest : public ::testing::Test {
+protected:
+  using V1_t = typename Tp_t::first_type;
+  using V2_t = typename Tp_t::second_type;
+  // element types must of course be the same
+  static_assert(
+    std::is_same_v<typename V1_t::value_type, typename V2_t::value_type>,
+    "V1_t::value_type, V2_t::value_type must be the same"
+  );
+  static const V1_t values_1_;
+  static const V2_t values_2_;
+};
+
+template <typename Tp_t>
+const typename Tp_t::first_type
+HelpersVtTwoTest<Tp_t>::values_1_{{HELPERS_TEST_VECTOR_VALUES}};
+
+template <typename Tp_t>
+const typename Tp_t::second_type
+HelpersVtTwoTest<Tp_t>::values_2_{{HELPERS_TEST_VECTOR_VALUES}};
+
+using HelpersVtTwoTestTypes = ::testing::Types<
+  std::pair<pdmath::double_vector, Eigen::VectorXd>,
+  std::pair<Eigen::VectorXf, pdmath::float_vector>
+>;
+TYPED_TEST_SUITE(HelpersVtTwoTest, HelpersVtTwoTestTypes);
 
 }  // namespace
