b0bac3f78c0e9552475c92a69480828a803e1cca
Derek Huang <djh458@stern.nyu.edu>
2022-10-16 20:51:34 -0400

    line_search_test.cc: make steepest_search_ and backtrack_step_ members non-static (their state mutates with each operator() call), add test for steepest_direction_search<V_t>

--- test/line_search_test.cc
+++ test/line_search_test.cc
@@ -11,8 +11,8 @@
 #include <gtest/gtest.h>
 
 #include "pdmath/helpers.h"
-#include "pdmath/norms.h"
 #include "pdmath/math_functors.h"
+#include "pdmath/norms.h"
 #include "pdmath/testing/macros.h"
 #include "pdmath/testing/utils.h"
 
@@ -21,7 +21,7 @@ namespace {
 /**
  * Test fixture for our line search tests.
  *
- * @tparam Tp_t A specialization of `func_type_pair`
+ * @tparam Tp_t `func_type_pair` specialization
  */
 template <typename Tp_t>
 class LineSearchTest
@@ -32,32 +32,46 @@ public:
   using hessian_type = typename Tp_t::hessian_type;
 
 protected:
-  // functor to minimize (chosen for convenience)
-  static inline pdmath::himmelblau_functor<gradient_type, hessian_type> himmel_;
-  // direction search using steepest (gradient) descent
-  static inline pdmath::steepest_direction_search<gradient_type> steepest_dir_{
-    // note no capture needed, as himmel_ is static. lambda is used as a way to
-    // get around member functions being different types than normal functions.
-    [](const gradient_type& x) { return himmel_.d1(x); }
-  };
-  // step search policies: constant step size, backtrack search
-  static inline pdmath::const_step_search<gradient_type> const_step_;
+  /**
+   * Constructor to populate non-shared members that will have state mutation.
+   */
+  LineSearchTest()
+    : steepest_search_{
+        // note no capture needed, as himmel_ is static. lambda is used to get
+        //around member functions being different types than normal functions.
+        [](const gradient_type& x) { return himmel_.d1(x); }
+      },
 // MSVC complains that double (0.1) is being truncated to float
 #ifdef _MSC_VER
 #pragma warning (push)
 #pragma warning (disable: 4305)
 #endif  // _MSC_VER
-  static inline pdmath::backtrack_step_search<gradient_type> backtrack_step_{
-    [](const gradient_type& x) { return himmel_.f(x); },
-    [](const gradient_type& x) { return himmel_.d1(x); },
-    0.1
-  };
+      backtrack_step_{
+        [](const gradient_type& x) { return himmel_.f(x); },
+        [](const gradient_type& x) { return himmel_.d1(x); },
+        0.1
+      }
 #ifdef _MSC_VER
 #pragma warning (pop)
 #endif  // _MSC_VER
+  {}
+
+  // direction search using steepest (gradient) descent
+  pdmath::steepest_direction_search<gradient_type> steepest_search_;
+  // step search policy using backtracking step search
+  pdmath::backtrack_step_search<gradient_type> backtrack_step_;
+
+  // functor to minimize (chosen for convenience)
+  static inline pdmath::himmelblau_functor<gradient_type, hessian_type> himmel_;
+  // step search policy using constant step size
+  static inline pdmath::const_step_search<gradient_type> const_step_;
   // line search direction convergence policies: no early convergence, 2-norm
   static inline pdmath::no_direction_policy<gradient_type> no_policy_;
   static inline pdmath::min_p_norm_direction_policy<gradient_type> min_policy_;
+  // starting point of (0, 0)
+  static inline const gradient_type x0_{{0, 0}};
+  // first Himmelblau minimum
+  static inline const gradient_type min_1_{{HIMMELBLAU_ZERO_1}};
 };
 
 // types LineSearchTest will be instantiated with + register types
@@ -68,6 +82,22 @@ using LineSearchTestTypes = ::testing::Types<
   pdmath::testing::func_type_pair<pdmath::vector_d, Eigen::Matrix2d>
 >;
 TYPED_TEST_SUITE(LineSearchTest, LineSearchTestTypes);
+
+/**
+ * Test that the `steepest_direction_search` functor works as expected.
+ *
+ * Uses `HIMELBLAU_ZERO_1` to check that the gradient is zero (should actually
+ * be exactly zero) and checks function, gradient, Hessian evals.
+ */
+TYPED_TEST(LineSearchTest, SteepestDirectionSearchTest)
+{
+  EXPECT_THAT(
+    this->steepest_search_(TestFixture::min_1_),
+    TestFixture::all_near_zero_matcher
+  );
+  // should increase gradient eval count to 1
+  EXPECT_EQ(1, this->steepest_search_.n_gev());
+}
 
 TYPED_TEST(LineSearchTest, DISABLED_HimmelblauTest)
 {
