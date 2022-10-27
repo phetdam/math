42c71018a40bd78c6085de385dd9b506f7a23a35
Derek Huang <djh458@stern.nyu.edu>
2022-10-27 02:52:19 -0400

    type_traits_test.cc: add test for has_operator_string

--- include/pdmath/type_traits.h
+++ include/pdmath/type_traits.h
@@ -8,7 +8,9 @@
 #ifndef PDMATH_TYPE_TRAITS_H_
 #define PDMATH_TYPE_TRAITS_H_
 
+#include <string>
 #include <type_traits>
+#include <utility>
 
 namespace pdmath {
 
@@ -39,6 +41,30 @@ struct is_vector_size_constructible<T, std::void_t<typename T::Index>>
 template <typename T>
 inline constexpr bool
 is_vector_size_constructible_v = is_vector_size_constructible<T>::value;
+
+/**
+ * Checks if a type is castable to `std::string`.
+ *
+ * That is, the type `T` implements or inherits `operator std::string`, which
+ * may or may not be declared as explicit, i.e. cast required.
+ *
+ * @param T type
+ */
+template <typename T, typename = void>
+struct has_operator_string : std::false_type {};
+
+template <typename T>
+struct has_operator_string<
+  T, std::void_t<decltype(std::declval<T>().operator std::string())>>
+  : std::true_type {};
+
+/**
+ * Helper for `has_operator_string` to get the truth value.
+ *
+ * @tparam T type
+ */
+template <typename T>
+inline constexpr bool has_operator_string_v = has_operator_string<T>::value;
 
 }  // namespace
 
--- test/type_traits_test.cc
+++ test/type_traits_test.cc
@@ -11,6 +11,7 @@
 #include <mutex>
 #include <string>
 #include <type_traits>
+#include <typeinfo>
 
 #include <Eigen/Core>
 #include <gtest/gtest.h>
@@ -56,6 +57,61 @@ TYPED_TEST_SUITE(
 TYPED_TEST(TypeTraitsSizeConstructibleTest, TruthTest)
 {
   EXPECT_META_TEMPLATE_TRUTH_EQ(pdmath::is_vector_size_constructible_v);
+}
+
+/**
+ * Template class castable to a `std::string` giving its mangled `typeid` name.
+ *
+ * @tparam T type
+ */
+template <typename T>
+class printable {
+public:
+  operator std::string() const { return typeid(T).name(); }
+};
+
+/**
+ * Class [implicitly] castable to a `std::string` containing `"hello"`.
+ */
+class implicit_hello {
+public:
+  operator std::string() const { return "hello"; }
+};
+
+/**
+ * Class castable to a `std::string` containing `"hello"`, but only explicitly.
+ */
+class explicit_hello {
+public:
+  explicit operator std::string() const { return "hello"; }
+};
+
+/**
+ * Test fixture template for testing `is_stringable`.
+ *
+ * @tparam Tp_t `type_value_pair<T, v>` specialization
+ */
+template <typename Tp_t>
+class TypeTraitsHasOperatorStringTest : public ::testing::Test {};
+
+using TypeTraitsHasOperatorStringTestTypes = ::testing::Types<
+  pdmath::testing::type_value_pair<printable<pdmath::array_pair_f>, true>,
+  pdmath::testing::type_value_pair<implicit_hello, true>,
+  pdmath::testing::type_value_pair<explicit_hello, true>,
+  pdmath::testing::type_value_pair<pdmath::boost_vector_d, false>,
+  pdmath::testing::type_value_pair<std::list<int>, false>,
+  pdmath::testing::type_value_pair<std::mutex, false>
+>;
+TYPED_TEST_SUITE(
+  TypeTraitsHasOperatorStringTest, TypeTraitsHasOperatorStringTestTypes
+);
+
+/**
+ * Check that `has_operator_string` is substituting correctly.
+ */
+TYPED_TEST(TypeTraitsHasOperatorStringTest, TruthTest)
+{
+  EXPECT_META_TEMPLATE_TRUTH_EQ(pdmath::has_operator_string_v);
 }
 
 }  // namespace
