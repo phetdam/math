b477d234e4d8db872f7ae04aadeeb40d01faa39d
Derek Huang <djh458@stern.nyu.edu>
2025-10-02 23:45:00 -0400

    type_traits.h: cleanup + add constraint<V> traits type

--- include/pdmath/type_traits.h
+++ include/pdmath/type_traits.h
@@ -39,7 +39,7 @@ struct is_vector_size_constructible<T, std::void_t<typename T::Index>>
  * @tparam T type
  */
 template <typename T>
-inline constexpr bool
+constexpr bool
 is_vector_size_constructible_v = is_vector_size_constructible<T>::value;
 
 /**
@@ -48,7 +48,7 @@ is_vector_size_constructible_v = is_vector_size_constructible<T>::value;
  * That is, the type `T` implements or inherits `operator std::string`, which
  * may or may not be declared as explicit, i.e. cast required.
  *
- * @param T type
+ * @tparam T type
  */
 template <typename T, typename = void>
 struct has_operator_string : std::false_type {};
@@ -64,8 +64,44 @@ struct has_operator_string<
  * @tparam T type
  */
 template <typename T>
-inline constexpr bool has_operator_string_v = has_operator_string<T>::value;
+constexpr bool has_operator_string_v = has_operator_string<T>::value;
 
-}  // namespace
+/**
+ * Constraint traits type.
+ *
+ * This is inspired by something similar in Boost.Asio that enables more
+ * unified implementation of pre-C++20 template constraints instead of using
+ * SFINAE with template type parameters. This avoids the following:
+ *
+ *  - `std::enable_if_t` being verbose with return types + not supporting `auto`
+ *  - `std::enable_if_t` being used to constrain function templates via default
+ *    template type parameter. The compiler will see this as a redeclaration of
+ *    the same function template. See the cppreference documentation for
+ *    details: https://en.cppreference.com/w/cpp/types/enable_if.html
+ *
+ * See `constraint_t` for an example of how to use the constraint traits.
+ *
+ * @tparam V `constexpr` truth value
+ */
+template <bool V>
+struct constraint {};
+
+/**
+ * SFINAE helper for using the constraint traits with function templates.
+ *
+ * @tparam V `constexpr` truth value
+ */
+template <bool V>
+using constraint_t = typename constraint<V>::type;
+
+/**
+ * True specialization that provides the `int` type alias.
+ */
+template <>
+struct constraint<true> {
+  using type = int;
+};
+
+}  // namespace pdmath
 
 #endif  // PDMATH_TYPE_TRAITS_H_
