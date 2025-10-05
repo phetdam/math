350b27feaa4a08a14132dd2c39d0bcf7f1d81cc7
Derek Huang <djh458@stern.nyu.edu>
2025-10-05 03:01:35 -0400

    type_traits.h: add is_ostreamable, is_range traits + helpers + add missing constraint_t examples

--- include/pdmath/type_traits.h
+++ include/pdmath/type_traits.h
@@ -8,6 +8,8 @@
 #ifndef PDMATH_TYPE_TRAITS_H_
 #define PDMATH_TYPE_TRAITS_H_
 
+// TODO: consider using <iosfwd> and std::basic_ostream<char> instead
+#include <ostream>
 #include <string>
 #include <type_traits>
 #include <utility>
@@ -89,6 +91,31 @@ struct constraint {};
 /**
  * SFINAE helper for using the constraint traits with function templates.
  *
+ * This is more convenient that `std::enable_if_t` since it avoids the pitfalls
+ * detailed in the `constraint` documentation. It can be used the same way as
+ * its Boost.Asio analogue; for example:
+ *
+ * @code{.cc}
+ * template <typename T>
+ * auto float_only(T v, constraint_t<std::is_floating_point_v<T>> = 0)
+ * {
+ *   return v + 1;
+ * }
+ * @endcode
+ *
+ * In cases where an optional argument is not allowed or not desirable, one can
+ * use it like `std::enable_if_t` as a non-type template parameter:
+ *
+ * @code{.cc}
+ * // constrain to only floating-point value types while allowing additional
+ * // constrained function templates, e.g. for integral value types
+ * template <typename T, typename A, constraint_t<std::is_floating_point_v<T>> = 0>
+ * auto& operator<<(std::ostream& out, const std::vector<T, A>& vec)
+ * {
+ *   // ...
+ * }
+ * @endcode
+ *
  * @tparam V `constexpr` truth value
  */
 template <bool V>
@@ -101,6 +128,65 @@ template <>
 struct constraint<true> {
   using type = int;
 };
+
+/**
+ * Traits to indicate if a type can be streamed to `std::ostream`.
+ *
+ * @tparam T type
+ */
+template <typename T, typename = void>
+struct is_ostreamable : std::false_type {};
+
+/**
+ * True specialization for a type that is streamable to `std::ostream`.
+ *
+ * @tparam T type
+ */
+template <typename T>
+struct is_ostreamable<
+  T, std::void_t<decltype(std::declval<std::ostream>() << std::declval<T>())> >
+  : std::true_type {};
+
+/**
+ * Indicate if a type is streamable to `std::ostream`.
+ *
+ * @tparam T type
+ */
+template <typename T>
+constexpr bool is_ostreamable_v = is_ostreamable<T>::value;
+
+/**
+ * Traits type to indicate a pre-C++20 range.
+ *
+ * This is a very weak specification of a range where the type needs to only
+ * provide start and end iterators via `std::begin` and `std::end`.
+ *
+ * @todo Strengthen so we actually check that iterators are returned.
+ *
+ * @tparam T type
+ */
+template <typename T, typename = void, typename = void>
+struct is_range : std::false_type {};
+
+/**
+ * True specialization for a type that provides begin + end iterators.
+ *
+ * @tparam T type
+ */
+template <typename T>
+struct is_range<
+  T,
+  std::void_t<decltype(std::begin(std::declval<T>()))>,
+  std::void_t<decltype(std::end(std::declval<T>()))>
+> : std::true_type {};
+
+/**
+ * Indicate that a type is a range-like type.
+ *
+ * @tparam T type
+ */
+template <typename T>
+constexpr bool is_range_v = is_range<T>::value;
 
 }  // namespace pdmath
 
