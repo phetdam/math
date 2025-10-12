abbde1b3df3577c6dc63f2e3e66787879f0b29e6
Derek Huang <djh458@stern.nyu.edu>
2025-10-12 17:40:32 -0400

    rename fib50.cc to fib_50.cc
    modified:   src/CMakeLists.txt
    new file:   src/fib_50.cc

---
+++ src/fib_50.cc
@@ -0,0 +1,21 @@
+/**
+ * @file fib_50.cc
+ * @author Derek Huang
+ * @brief C++ program that prints the first 50 Fibonacci numbers
+ * @copyright MIT License
+ *
+ * The definition of Fibonacci numbers we use starts with F(0) = 0 to match the
+ * OEIS list of the first 2000 Fibonacci numbers. So Fibonacci number 50 is
+ * F(49), or 7778742049, which requires more than 32 bits to store.
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+#include "pdmath/fibonacci.h"
+
+int main()
+{
+  std::cout << pdmath::fibonacci_sequence<50u - 1u>{} << std::endl;
+  return EXIT_SUCCESS;
+}
