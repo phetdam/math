20ff35aa250d1491949214980d459e81f5c0f5c4
Derek Huang <djh458@stern.nyu.edu>
2025-10-12 14:40:14 -0400

    features.h: add feature macros for C++ standard detection

--- include/pdmath/features.h
+++ include/pdmath/features.h
@@ -8,6 +8,36 @@
 #ifndef PDMATH_FEATURES_H_
 #define PDMATH_FEATURES_H_
 
+#include "pdmath/common.h"
+
+// C++ standards support
+#if defined(PDMATH_CPLUSPLUS)
+// C++17
+#if PDMATH_CPLUSPLUS >= 201703L
+#define PDMATH_HAS_CXX17 1
+#endif  // PDMATH_CPLUSPLUS >= 201703L
+// C++20
+#if PDMATH_CPLUSPLUS >= 202002L
+#define PDMATH_HAS_CXX20 1
+#endif  // PDMATH_CPLUSPLUS >= 202002L
+// C++23
+#if PDMATH_CPLUSPLUS >= 202302L
+#define PDMATH_HAS_CXX23 1
+#endif  // PDMATH_CPLUSPLUS >= 202302L
+#endif  // defined(PDMATH_CPLUSPLUS)
+
+#ifndef PDMATH_HAS_CXX17
+#define PDMATH_HAS_CXX17 0
+#endif  // PDMATH_HAS_CXX17
+
+#ifndef PDMATH_HAS_CXX20
+#define PDMATH_HAS_CXX20 0
+#endif  // PDMATH_HAS_CXX20
+
+#ifndef PDMATH_HAS_CXX23
+#define PDMATH_HAS_CXX23 0
+#endif  // PDMATH_HAS_CXX23
+
 // Eigen 3
 #ifdef __has_include
 // check Eigen 3 signature for surest check
