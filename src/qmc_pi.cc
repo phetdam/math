40a877ba38f211e625051147e4e7d9af8edf8d66
Derek Huang <djh458@stern.nyu.edu>
2025-10-24 08:04:57 -0400

    qmc_pi.cc: use scoped timer + report execution time

--- src/qmc_pi.cc
+++ src/qmc_pi.cc
@@ -9,12 +9,22 @@
 #include <iostream>
 
 #include "pdmath/pi.h"
+#include "pdmath/scoped_timer.h"
 
 int main()
 {
   // number of [0, 1] partitions to make
   constexpr auto n = 5000u;
+  // compute time
+  std::chrono::milliseconds time;
   // estimate using n * n samples
-  std::cout << "pi (n_points=" << n * n << "): " << pdmath::qmc_pi(n) << std::endl;
+  auto pi_hat = [&time]
+  {
+    pdmath::scoped_timer timer{time};
+    return pdmath::qmc_pi(n);
+  }();
+  // print
+  std::cout << "pi (n_points=" << n * n << "): " << pi_hat << " in " <<
+    time.count() << " ms" << std::endl;
   return EXIT_SUCCESS;
 }
