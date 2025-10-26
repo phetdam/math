a79cc2844a9538013a054f1f6c333665fe1ab28e
Derek Huang <djh458@stern.nyu.edu>
2025-10-25 23:47:12 -0400

    vtk_axis.h: add functionality to show/hide grid lines and set tick count and length

--- include/pdmath/vtk_axis.h
+++ include/pdmath/vtk_axis.h
@@ -48,6 +48,81 @@ public:
   }
 
   /**
+   * Indicate if the grid lines are visible or not.
+   */
+  bool grid_visible() const
+  {
+    return object()->GetGridVisible();
+  }
+
+  /**
+   * Indicate if the grid lines should be visible or not.
+   *
+   * @param v `true` for grid lines to be visible, `false` to hide
+   */
+  auto& grid_visible(bool v)
+  {
+    object()->SetGridVisible(v);
+    return *this;
+  }
+
+  /**
+   * Hide the grid lines.
+   */
+  auto& hide_grid()
+  {
+    return grid_visible(false);
+  }
+
+  /**
+   * Show the grid lines.
+   */
+  auto& show_grid()
+  {
+    return grid_visible(true);
+  }
+
+  /**
+   * Get the axis tick length in fractional pixels.
+   */
+  auto tick_length() const
+  {
+    return object()->GetTickLength();
+  }
+
+  /**
+   * Set the axis tick length in fractional pixels.
+   *
+   * @param len Tick length in fractional pixels
+   */
+  auto& tick_length(float len)
+  {
+    return object()->SetTickLength(len);
+    return *this;
+  }
+
+  /**
+   * Get number of tick marks for the axis.
+   */
+  auto ticks() const
+  {
+    return object()->GetNumberOfTicks();
+  }
+
+  /**
+   * Set the number of tick marks for the axis.
+   *
+   * If set to `-1` the tick mark calculation is done automatically.
+   *
+   * @param n Number of ticks to mark on the axis or `-1`
+   */
+  auto& ticks(int n)
+  {
+    object()->SetNumberOfTicks(n);
+    return *this;
+  }
+
+  /**
    * Get the axis title string.
    */
   auto title() const
